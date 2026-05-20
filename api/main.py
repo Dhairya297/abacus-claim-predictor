978a504072205fa39a1c4f4a2bf3cfca17a19347
Dhairya297 <dhairyashimpi2907@gmail.com>
2026-05-21 04:36:58 +0530

    api url and frontend url issues fixed

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes.predict import router as predict_router
 
@@ -7,10 +8,16 @@ app = FastAPI(
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(predict_router)
 
 @app.get("/")
 def health_check():
-    return {
-        "status": "running"
-    }
+    return {"status": "running"}
